ae917e44b193c7710a9695d5c3c0a64998db85aa
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-10-06 17:58:46 +0300

    .

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -6,7 +6,27 @@ namespace Lesson9
     {
         static void Main(string[] args)
         {
-            string name = null;
+
+
+            //- מחרוזת עם רווח בהתחלה  לקלוט מהשתמש מחרוזת באנגלית 
+            //לעשות הורדת רווח בהתחלה 
+            //להדפיס את המחרוזת
+            //לקלוט מחרוזת עם רווח בסוף ולהוריד את הרווח 
+            //להדפיס את המחרוזת
+            //לקלוט מחרוזת עם רווח בהתחלה וגם בסוף 
+            //למחוק את הרווחים בהתחלה ובסוף
+            //ולהדפיס את המחרוזת
+
+            
+            //string str1 = " yar in ";
+            ////str1 = str1.TrimStart();
+            //str1 = str1.Trim(); 
+            ////str1 = str1.TrimEnd();
+            //string str2 = "gg";
+            //Console.WriteLine(str1+str2);
+            //Console.WriteLine(name);
+            ////Console.WriteLine(name.TrimStart());
+            //Console.WriteLine(name.TrimEnd());
             //Console.WriteLine(name[0]);
             //Console.WriteLine(name[name.Length-1]);
             //for (int i = 0; i < name.Length; i++)
